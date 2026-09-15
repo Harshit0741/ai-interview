6ec1f4ca57451de2e1ab62fd8d6e8ce375e5e3b3
Harshit <137671416+Harshit0741@users.noreply.github.com>
2026-09-15 21:44:18 +0530

    Update page.jsx

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -9,7 +9,7 @@ function Login() {
     const {error} = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_HOST_URL}/dashboard` 
+        redirectTo: `${process.env.NEXT_PUBLIC_BASIC_URL}/dashboard` 
       }
     })
     if(error){
